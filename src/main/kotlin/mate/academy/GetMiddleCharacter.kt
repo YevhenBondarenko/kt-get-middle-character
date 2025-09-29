d8ca0dccaeef4750966c30f99c65eb3342c1c097
YevhenBondarenko <bondarenkojek@gmail.com>
2025-09-29 22:55:49 +0200

    implemented  getMiddleString function

--- src/main/kotlin/mate/academy/GetMiddleCharacter.kt
+++ src/main/kotlin/mate/academy/GetMiddleCharacter.kt
@@ -3,6 +3,8 @@ package mate.academy
 // implement the `getMiddleString()` function, which takes a string
 // and returns middle character (or characters) as a string
 
-fun getMiddleString(originalString: String): String {
-    return ""
+fun getMiddleString(s: String): String = when {
+    s.isEmpty() -> s
+    s.length % 2 == 0 -> s.substring(s.length / 2 - 1, s.length / 2 + 1)
+    else -> s[s.length / 2].toString()
 }
